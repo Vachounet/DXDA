2e3b5f814c5c6571370e18a278a2828305391429
Vachounet <vachounet@live.fr>
2017-07-27 15:25:30 +0200

    Fix PM helpers

--- client/views/pm/pm.js
+++ client/views/pm/pm.js
@@ -1,5 +1,13 @@
 Template.pm.helpers({
     getDate: function (timestamp) {
         return new Date(timestamp * 1000).toUTCString();
-    },    
+    },
+    bbtohtml: function (text) {
+        var result = XBBCODE.process({
+            text: text,
+            removeMisalignedTags: true,
+            addInLineBreaks: true
+        });
+        return result.html;
+    }, 
 })
