c448906713db827d8fa66e8dac52adee40801c20
Vachounet <vachounet@live.fr>
2017-08-22 09:21:21 +0200

    allow refreshing subscribed forums

--- client/views/subscribed/subscribed.js
+++ client/views/subscribed/subscribed.js
@@ -3,11 +3,25 @@
 Template.subscribed.helpers({
     subscribeds() {
         return Session.get("subscribeds");
-    }
+    },
+        isProcessing() {
+        return Session.get("showLoadingSpinner");
+
+    },
 })
 
+Template.subscribed.events({
+    'click #refresh' (event, instance) {
+        Session.set("showLoadingSpinner", true);
+        Meteor.call('getSubscription', document.cookie, (error, result) => {
+            Session.set("subscribeds", result.data.results);
+            Session.set("showLoadingSpinner", false);
+        });
+    },
+});
+
 Template.subscribed.onRendered(function helloOnCreated() {
-Session.set("showLoadingSpinner", false);
+    Session.set("showLoadingSpinner", false);
 });
 
 Template.subscribed.onCreated(function helloOnCreated() {
