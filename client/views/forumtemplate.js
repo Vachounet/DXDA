65373a03145e3c9d8c293b05536774df42bf9901
Vachounet <vachounet@live.fr>
2017-08-22 09:19:06 +0200

    display unread icons when needed for all forums

--- client/views/forumtemplate.js
+++ client/views/forumtemplate.js
@@ -1,27 +1,35 @@
-import { checkbox, ripple, iconToggle } from 'meteor/zodiase:mdc-styleless';
+import {
+    checkbox,
+    ripple,
+    iconToggle
+} from 'meteor/zodiase:mdc-styleless';
 
 Template.forumtemplate.onRendered(function helloOnCreated() {
-  this.$('[data-interactive-list]').each((index, element) => {
-    ripple.MDCRipple.attachTo(element);
-  });
+    this.$('[data-interactive-list]').each((index, element) => {
+        ripple.MDCRipple.attachTo(element);
+    });
 });
 
 Template.forumtemplate.helpers({
-    getPicURL: function(url){
+    getPicURL: function (url) {
         return "https://forum.xda-developers.com/images/forumpics_new/" + url.split("/")[url.split("/").length - 1];
-    }
+    },
+    unreads: function (val) {
+        return val === 1
+    },
 });
 
 Template.forumtemplate.events({
     'click .mdc-list-item' (event, instance) {
-        //console.log(this)
+        console.log(this)
         if (this.children.length > 0) {
-        Session.set("currentSubForums", this.children)
-        Session.set("previousPage", "/top")
-        Session.set("currentPageTitle", this.title)
-        Router.go("subs");
-    } else {
-        Router.go("/threads/" + this.forumid);
+            Session.set("currentSubForums", this.children)
+            Session.set("previousPage", "/top")
+            Session.set("currentPageTitle", this.title)
+            Router.go("subs");
+        } else {
+            Session.set("currentPageTitle", this.title)
+            Router.go("/threads/" + this.forumid);
+        }
     }
-}
 });
